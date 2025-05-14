272f5639d3e93c01d718dab352d2bcb3fe0be2da
name22334 <quang20042204@gmail.com>
2025-05-14 09:15:09 +0700

    file

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,18 +1,18 @@
-import {
-    RouterProvider,
-    createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layoutcline from "../layout/layoutcline";
-import Home from "../pages/Home";
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layoutcline />,
-        children: [{
-            index: true, element: <Home />
-        }]
-    }
-])
-// export const Router: React.FC = () => {
-//     return <RouterProvider router={router} />;
-// };
+import ProductDetail from "../pages/client/ProductDetail";
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layoutcline />,
+    children: [
+      {
+        path: '/sanPham',
+        element: <ProductDetail />,
+      },
+    ],
+  },
+]);
+export const Routes: React.FC = () => {
+  return <RouterProvider router={routes} />;
+};
