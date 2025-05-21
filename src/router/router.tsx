c45479ad4fee5ea13587aaea0f4312769cd53c2a
quangluongw <quang20042204@gmail.com>
2025-05-21 08:58:09 +0700

    layout account

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import Cart from "../pages/Cart/Cart";
 import CheckOut from "../pages/Check Out/CheckOut";
 import Notfound from "../pages/404";
+import Account from "../pages/Account/Account";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
       {
         path: "checkout",
         element: <CheckOut />,
+      },
+      {
+        path: "accout",
+        element: <Account />,
       },
       {
         path:"*",
